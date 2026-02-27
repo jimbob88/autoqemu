9075e0468914f1a77c36a8ca8fe52614d0cc4019
James Blackburn <blackburnfjames@gmail.com>
2026-02-27 13:16:39 +0000

    feat: add qmp connection and dump

--- src/autoqemu/__init__.py
+++ src/autoqemu/__init__.py
@@ -1,2 +1,50 @@
+from pathlib import Path
+from autoqemu.dumper import connect, dump_client
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="autoqemu", description="Launches kernels and dumps the memory image"
+    )
+    parser.add_argument(
+        "-k",
+        "--kernel",
+        type=Path,
+        help="The bzImage used to launch the kernel",
+        required=True,
+    )
+    parser.add_argument(
+        "-r",
+        "--rootfs",
+        type=Path,
+        help="The initrd root filesystem to boot from",
+        required=True,
+    )
+    parser.add_argument(
+        "-f",
+        "--dump-file",
+        type=Path,
+        help="Where to dump the file",
+        default=Path("./memory_dump.img"),
+    )
+    parser.add_argument(
+        "-s",
+        "--qmp-socket",
+        help="The socket to run the QMP server on",
+        default="qmp.sock",
+    )
+    parser.add_argument(
+        "-n",
+        "--qmp-nickname",
+        help="The name of the qmp connection client",
+        default="qmp-dld",
+    )
+    return parser.parse_args()
+
+
 def main() -> None:
     print("Hello from autoqemu!")
+    args = parse_args()
+    qmp_client = connect(args.qmp_nickname, args.qmp_socket)
+    dump_client(qmp_client, args.dump_file)
---
+++ src/autoqemu/dumper.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from qemu.qmp import QMPClient
+
+
+async def connect(nickname: str, socket: str) -> QMPClient:
+    qmp = QMPClient(nickname)
+    await qmp.connect(socket)
+    return qmp
+
+
+async def dump_client(client: QMPClient, out_file: Path) -> dict:
+    return client.execute(
+        "dump-guest-memory",
+        arguments={"paging": "false", "protocol": f"file:{out_file.absolute()}"},
+    )
